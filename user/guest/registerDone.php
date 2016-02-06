afa435e22ec0a33e3be07dcc6a44d3cdb1c6958a
Eugene Grava <eugenegrava.c@gmail.com>
2016-02-07 03:28:30 +0800

    Modified registerDone.php for the fading well
    Signed-off-by: Eugene Grava <eugenegrava.c@gmail.com>

--- user/guest/registerDone.php
+++ user/guest/registerDone.php
@@ -1,10 +1,5 @@
 <?php
-	ob_start(); // para sa warning na cannont modify header...
-	/*include('guestLink.php');
-	include ('../../include/header.php');
-	include ('guestNav.php');
-	include ('../../config/connection.php');
-	*/
+	ob_start(); 
 	$root = realpath(dirname(__FILE__) . '/../..');
 	//include($root . '/include/link.php');
 	include($root . '/include/header.php');
@@ -12,12 +7,9 @@
 	include ('guestNav.php'); // native to guest
 ?>
 	
-	
-	
 
-
-		<div class="container-fluid" style="margin-top:0em; margin-bottom:15em;">
-			<div class='alert' id='hi'>
+		<div class="container-fluid" style="margin-top:5em; margin-bottom:10em;">
+			<div class='alert' id='hello'>
 						<div class='alert-success well-lg col-md-8 col-md-offset-2'>
 							<h3 style='text-align:center;'>
 								Congratulations! You've successfully registered. <br /><br />
